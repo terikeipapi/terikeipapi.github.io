db80af79d3d648dada20cc5d12fcd05dfed710d2
김태경 <rlaxorud0131@naver.com>
2026-05-19 19:35:26 +0900

    feat : quotes 내용 추가

--- js/quotes.js
+++ js/quotes.js
@@ -2,6 +2,26 @@ const quotes = [
   {
     quote: "이럴 때일수록 내 이미지를 위해선 그저 so cool한 척 하는게 최선",
     author: "빈지노 - Aqua Man",
+  },
+  {
+    quote: "I'll give you time and space, just know I'm not a phase",
+    autho: "Daniel Caesar - always"
+  },
+  {
+    quote: "Chances are I'll step out of line, but who knows?",
+    autho: "Daniel Caesar - Who Knows"
+  },
+  {
+    quote: "우린 같은 템포, 다른 노래인 거야",
+    autho: "검정치마 - 한시 오분 (1:05)"
+  },
+  {
+    quote: "실처럼 가늘 때에도 절대로 엉키지 않아",
+    autho: "검정치마 - Big Love"
+  },
+  {
+    quote: "마지막 폭죽이 터지는 그날에도 별로 슬퍼할 것 같진 않아",
+    autho: "검정치마 - 폭죽과 풍선들"
   }
 ];
 
