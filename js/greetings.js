ef0d878005cf966afbf6464fbd9c08e180e0d35d
김태경 <rlaxorud0131@naver.com>
2026-05-19 19:02:22 +0900

    feat : greeting 새로고침 오류 해결

--- js/greetings.js
+++ js/greetings.js
@@ -1,5 +1,5 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
@@ -14,9 +14,9 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  // 수정된 부분: 고정된 텍스트 대신 입력받은 username 변수를 출력하도록 변경
   greeting.innerText = `hallo ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
